23f189f87a08f2756ef6de5aa8cfdae4c6026a5f
Tbag00 <tommaso.bagiana@gmail.com>
2025-02-18 18:00:22 +0100

    proseguimento riconoscimento blocchi

--- testing.py
+++ testing.py
@@ -4,42 +4,39 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import datasets, models, layers, callbacks
 import matplotlib.pyplot as plt
-import cv2
+import cv2 as cv
 
-cv2.ocl.setUseOpenCL(False)
+cv.ocl.setUseOpenCL(False)
 model: models.Sequential = models.load_model("recognition_numbers.keras")
 
-img = cv2.imread('/home/tommaso/intelligenzaArtificiale/progetto/test_personali_numeri/2.jpeg', cv2.IMREAD_GRAYSCALE)
+img = cv.imread('/home/tommaso/intelligenzaArtificiale/progetto/test_personali_numeri/2.jpeg', cv.IMREAD_GRAYSCALE)
 
 assert img is not None, "file could not be read, check with os.path.exists()"
 # colori da invertire se uso OTSU inverto colori perché dataset ha colori invertiti
-#img = cv2.bitwise_not(img)
+#img = cv.bitwise_not(img)
 
 # miglioro contrasto
-img = cv2.normalize(
-    img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-(thresh, img) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.ADAPTIVE_THRESH_GAUSSIAN_C) # da' immagini invertite
-#(thresh, img) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+img = cv.normalize(
+    img, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
+(thresh, img) = cv.threshold(img, 128, 255, cv.THRESH_BINARY | cv.ADAPTIVE_THRESH_GAUSSIAN_C) # da' immagini invertite
+#(thresh, img) = cv.threshold(img, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
 
 # adatto input size
-img = cv2.resize(img,(28,28))
+img = cv.resize(img,(28,28))
 
 # pixel<128 settati a 0, pixel >128 settati a 255, THRESH_BINARY per avere immagine binaria, THRESH_OTSU
 
 print(img.shape)
 print("tipo di img: ", type(img))
 print(img)
-"""final_im = np.expand_dims(img, axis=2)
-print(final_im.shape)
-print(type(final_im.shape))"""
 print(model.predict(np.array([img])).argmax(axis=1))
 plt.imshow(img)
 plt.show()
-#cv2.imshow('image',img)
-#cv2.waitKey(0)
+#cv.imshow('image',img)
+#cv.waitKey(0)
 
 # closing all open windows
-# cv2.destroyAllWindows()
+# cv.destroyAllWindows()
 
 
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
@@ -50,18 +47,17 @@ print(model.predict(np.array([train_images[0]])))
 plt.imshow(train_images[0])
 plt.show()
 #guarda roi è la parte importante
-'''import cv2
+'''import cv
 import pytesseract
 
 # Carica l'immagine in scala di grigi
-img = cv2.imread("immagine.png", cv2.IMREAD_GRAYSCALE)
+img = cv.imread("immagine.png", cv.IMREAD_GRAYSCALE)
 
 # Trova i contorni
-contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)  # Bounding box del rettangolo
-    roi = img[y:y+h, x:x+w]  # Estrazione dell'area interna
+    x, y, w, h = cv.boundingRect(cnt)  # Bounding box del rettangolo
 
     # OCR per leggere il numero dentro il rettangolo
     numero = pytesseract.image_to_string(roi, config='--psm 6')
