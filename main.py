13551173c150df7bb58b1f5ba0cf010c76d09712
Tbag00 <tommaso.bagiana@gmail.com>
2025-02-23 14:59:38 +0100

    riconoscimento blocchi numeri e conversione in matrici ultimta e funzionante

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from tabnanny import verbose
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from tkinter import Label
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
@@ -9,6 +11,7 @@ from prova import Mproblem, Matrice, execute
 from puppolo import anima_matrice
 import cv2 as cv
 from modifica_matrice import edit_matrix
+from PIL import Image, ImageTk
 
 # Funzione per caricare l'immagine
 def upload_image(is_iniziale: bool):
@@ -31,7 +34,7 @@ def upload_image(is_iniziale: bool):
             immagine_finale = image
 
 # Funzione per mostrare l'immagine in una finestra separata usando matplotlib
-def show_image_in_window(image, is_iniziale):
+def show_image_in_window(image, is_iniziale: bool):
     # Converte l'immagine da BGR a RGB per matplotlib
     image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
@@ -43,15 +46,39 @@ def show_image_in_window(image, is_iniziale):
     plt.title(title)
     plt.show()
 
+# bottone verbose: se "on" mostra tutti i passaggi della ricognizione
+def verbose_toggle():
+    global verbose
+     
+    # Determine is on or off
+    if verbose:
+        verbose_toggle.config(image = off)
+        my_label.config(text = "Verbose non attivo", fg = "red")
+        verbose = False
+    else:
+        verbose_toggle.config(image = on)
+        my_label.config(text = "Verbose attivo", fg = "green")
+        verbose = True
+
+# Funzione per caricare l'immagine ridimensionata
+def load_resized_image(image_path, width, height):
+    img = Image.open(image_path)
+    img_resized = img.resize((width, height), Image.Resampling.LANCZOS)
+
+    # Converte l'immagine in formato compatibile con Tkinter
+    img_tk = ImageTk.PhotoImage(img_resized)
+    return img_tk
+
 # Funzione di conferma
-def confirm_images():
+def confirm_images(verbose: bool):
+    # controllo: entrambe immagini caricate
     if immagine_iniziale is None or immagine_finale is None:
         messagebox.showerror("Errore", "Devi caricare entrambe le immagini!")
         return
 
     # Prosegui con il calcolo e la visualizzazione
-    matrice_iniziale = getStato(immagine_iniziale)
-    matrice_finale = getStato(immagine_finale)
+    matrice_iniziale = getStato(immagine_iniziale, verbose)
+    matrice_finale = getStato(immagine_finale, verbose)
 
     print("matrice iniziale:")
     print(matrice_iniziale)
@@ -91,31 +118,45 @@ def exit_program():
 if __name__ == "__main__":
     immagine_iniziale = None
     immagine_finale = None
+    verbose = False
 
     # Crea la finestra principale
     root = tk.Tk()
     root.title("Block's World")
-    
+
     # Crea il frame per il layout
     frame = tk.Frame(root)
     frame.pack(padx=20, pady=20)
 
     root.protocol("WM_DELETE_WINDOW", on_close)
 
+    # Create Label
+    my_label = Label(root, 
+    text = "Verbose non attivo", 
+    fg = "red")
+    my_label.pack(pady = 20)
+    
+    on = load_resized_image("switch_png/on.png", 50, 50)  # Imposta la dimensione a 100x50 pixel
+    off = load_resized_image("switch_png/off.png", 50, 50)  # Imposta la dimensione a 100x50 pixel
+
     # Crea i pulsanti
     upload_button1 = tk.Button(frame, text="Upload Stato Iniziale", command=lambda: upload_image(is_iniziale=True))
-    upload_button1.grid(row=0, column=0, pady=10, padx=10)
+    upload_button1.pack(pady=10, padx=10)
 
     upload_button2 = tk.Button(frame, text="Upload Stato Finale", command=lambda: upload_image(is_iniziale=False))
-    upload_button2.grid(row=1, column=0, pady=10, padx=10)
+    upload_button2.pack(pady=10, padx=10)
 
     # Pulsante di conferma per procedere
-    confirm_button = tk.Button(frame, text="Conferma e Inizia", command=confirm_images)
-    confirm_button.grid(row=2, column=0, columnspan=2, pady=20)
+    confirm_button = tk.Button(frame, text="Conferma e Inizia", command=lambda: confirm_images(verbose))
+    confirm_button.pack(pady=20)
+
+    # Toggle verbose
+    verbose_toggle = tk.Button(root, image = off, bd = 0, command = verbose_toggle)
+    verbose_toggle.pack(pady=10, padx=10)
 
     # Pulsante di uscita
     exit_button = tk.Button(frame, text="Esci", command=exit_program)
-    exit_button.grid(row=3, column=0, columnspan=2, pady=20)
+    verbose_toggle.pack(pady=10, padx=10)
 
     # Avvia il loop principale
     root.mainloop()
--- recognition_blocks.py
+++ recognition_blocks.py
@@ -69,24 +69,19 @@ def getStato(original_img: cv.Mat, verbose: bool) -> np.array:
             print("lati:", len(approx))
             
             img_contoured: cv.Mat # usata solo se verbose
-            if verbose:
-                img_contoured = img.copy()
-                cv.drawContours(img_contoured, [contour], -1, (0,255,0), 10)
-                cv.drawContours(img_contoured, [approx], -1, (0,0,255), 10)
-                cv.imshow("Contour and approximated contour", img_contoured)
-                cv.waitKey(0)
 
             # controllo che contour sia rettangolo
             if len(approx) == 4:
                 x, y, w, h = cv.boundingRect(approx)
                 
-                if verbose: 
-                    cv.rectangle(img_contoured, (x, y), (x + w, y + h), (255, 0, 0), 10)
-                    cv.imshow("rectangle", img_contoured)
-                    cv.waitKey(0)
-
                 # salvo rettangoli con dimensioni limitate
                 if w > (larghezza_img / 16.0) and h > (altezza_img / 16.0) and w < (larghezza_img / 4.0) and h < (altezza_img / 4.0):
+                    if verbose: 
+                        img_contoured = img.copy()
+                        cv.rectangle(img_contoured, (x, y), (x + w, y + h), (255, 0, 0), 10)
+                        cv.imshow("rectangle", img_contoured)
+                        cv.waitKey(0)
+
                     rects.append({
                         "x": x, "y": y, "w": w, "h": h, "value": 0
                     })
@@ -157,12 +152,9 @@ def apply_gravity(matrix) -> np.matrix:
         matrix[:, col] = [0] * zero_count + non_zero_values  # Riempie con zeri sopra e numeri sotto
     return matrix
 
-
+"""
 def costruisci_mat(rects: list) -> np.matrix:
     n = len(rects)
-    """if n > 6:
-        print("tvoppe")
-        return"""
     mat = np.zeros((n, n, 2), dtype=int)  # Crea una matrice quadrata di zeri
 
     col = 0
@@ -189,87 +181,41 @@ def costruisci_mat(rects: list) -> np.matrix:
     res = res[::-1]  # ordine decrescente
 
     apply_gravity(res)
-    print(res)
     return res
-
-
-'''  # img = cv.imread('/home/tommaso/intelligenzaArtificiale/progetto/test_personali_blocks/img_ultrahd.jpeg', cv.IMREAD_GRAYSCALE)
-# img = cv.resize(img, (1024, 512))
-assert img is not None, "file could not be read, check with os.path.exists()"
-larghezza_img = img.shape[1]
-altezza_img = img.shape[0]
-'''
-
 """
-# miglioro contrasto
-cv.imshow("test",img)
-cv.waitKey(0)
-clahe = cv.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-cv.imshow("test",img)
-cv.waitKey(0)
-img=clahe.apply(img)
-blurred = cv.GaussianBlur(img, (21, 21), 0)
-img = cv.absdiff(img, blurred)
-img = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX)"""
-'''# kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
-# img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel, iterations=2)
-img = cv.normalize(
-    img, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
-(thresh, img) = cv.threshold(img, 128, 255, cv.THRESH_BINARY | cv.ADAPTIVE_THRESH_GAUSSIAN_C)
+def costruisci_mat(rects: list) -> np.matrix:
+    # Collezione per x raggruppa i rettangoli in colonne, le colonne hanno x molto vicina.
+    collezione_per_x = []
+    
+    # Raggruppa i rettangoli che appartengono allo stesso intervallo di x
+    for rect in rects:
+        trovato = False
+        for col in collezione_per_x:
+            # Verifica se il rettangolo appartiene a questa colonna
+            if (rect["x"] - rect["w"]/2 <= col[0]["x"] + col[0]["w"]/2) and (rect["x"] + rect["w"]/2 >= col[0]["x"] - col[0]["w"]/2):
+                col.append(rect)
+                trovato = True
+                break
+        
+        # Se non trovato, crea una nuova colonna per il nuovo gruppo di x
+        if not trovato:
+            collezione_per_x.append([rect])
+    
+    # Prepara la matrice finale con il numero massimo di rettangoli per colonna
+    # max_rects_in_col = max(len(col) for col in collezione_per_x)
+    # mat = np.zeros((max_rects_in_col, len(collezione_per_x)), dtype=int)
+    n = len(rects)
+    mat = np.zeros((n, n), dtype=int)  # Crea una matrice quadrata di zeri
+    
+    # Ordina i rettangoli per ogni gruppo in base a y e inseriscili nella matrice
+    for col_idx, col in enumerate(collezione_per_x):
+        # Ordina per y
+        col_sorted = sorted(col, key=lambda x: x["y"])
+        
+        # Inserisci nella colonna della matrice
+        for row_idx, rect in enumerate(col_sorted):
+            mat[row_idx, col_idx] = rect["value"]
 
-# decommenta se usi caussiana poché inverte i colori quindi li inverto
-img = cv.bitwise_not(img)
-
-# lista contenente i rettangoli
-# i rettangoli sono dizionari codificati dall' angolo in alto a sinistra (x,y) dalla base w e altezza h
-rects = []
-contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-cv.imshow("test", img)
-for contour in contours:
-    x, y, w, h = cv.boundingRect(contour)
-    ratio = w / float(h)
-
-    # controllo se la forma e' rettangolare
-    if 0.8 < ratio < 2.0:
-        # approssimo rettangolo
-        epsilon = 0.14 * cv.arcLength(contour, True)
-        approx = cv.approxPolyDP(contour, epsilon, True)
-
-        # controllo che contour sia rettangolo
-        if len(approx) == 4:
-            x, y, w, h = cv.boundingRect(approx)
-
-            # salvo rettangoli con dimensioni limitate
-            if w > (larghezza_img / 12) and h > (altezza_img / 12) and w < (larghezza_img / 4) and h < (
-                    altezza_img / 4):
-                cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 10)
-                cv.imshow("test2", img)
-                cv.waitKey(0)
-                rects.append({
-                    "x": x, "y": y, "w": w, "h": h, "value": 0
-                })
-
-# ordino i rettangoli da sinistra a destra
-rects = sorted(rects, key=lambda r: r["x"])
-
-# escludo rettangoli esterni
-inner_recs = copy.deepcopy(rects)
-for rectExt in rects:
-    exterior = False
-    for rectInt in rects:
-        if inside(rectExt, rectInt) and not rectExt is rectInt:
-            print("removed rectangle: %s" % rectExt["x"])
-            exterior = True
-    if exterior == False:
-        inner_recs.append(rectExt)
-
-for rect in inner_recs:
-    rect_img = img[rect["y"] + 10:rect["y"] + rect["h"] - 10, rect["x"] + 10:rect["x"] + rect["w"] - 10]
-    # cv.imshow("numero", rect_img)
-    # cv.waitKey(0)
-    rect["value"] = recon_number(rect_img)
-    print(rect["value"])
-
-costruisci_mat(rects)
-cv.destroyAllWindows()'''
+    apply_gravity(mat)
+    
+    return mat
